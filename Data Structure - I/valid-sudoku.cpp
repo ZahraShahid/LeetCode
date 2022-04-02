0a114179d0ad5bbc27b560cfe19d5e57ba851168
Zahra Shahid <zashahid45@gmail.com>
2022-04-03 02:39:59 +0500

    DS-I Day5

---
+++ Data Structure - I/valid-sudoku.cpp
@@ -0,0 +1,22 @@
+// Returns if the provide sudoku board is valid or not
+// https://leetcode.com/problems/valid-sudoku/
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<set<int>> rows(9), cols(9), blocks(9);
+        for(int i=0;i<9;++i){
+            for(int j=0;j<9;++j){
+                if(board[i][j]=='.') continue;
+                int curr=board[i][j];
+                //if value already present in the set, return false
+                if(rows[i].count(curr)||cols[j].count(curr)||blocks[(i/3*3)+j/3].count(curr)) return false;
+                //else insert the value
+                rows[i].insert(curr);
+                cols[j].insert(curr);
+                blocks[(i/3*3)+j/3].insert(curr);   
+            }
+        }
+        return true;
+    }
+};
