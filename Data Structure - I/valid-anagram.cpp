0ca5bad7d61f5a9a005586eaa69f8293afdc87f8
Zahra Shahid <zashahid45@gmail.com>
2022-04-04 23:27:12 +0500

    DS-I Day6

---
+++ Data Structure - I/valid-anagram.cpp
@@ -0,0 +1,12 @@
+// Returns if two of the given strings are Anagrams of each other.
+// https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
+        
+    }
+};
