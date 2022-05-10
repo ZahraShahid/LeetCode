2b978dc520ca54aa92c29422b5fd705aafe25720
Zahra Shahid <zashahid45@gmail.com>
2022-05-10 23:59:33 +0500

    D36

---
+++ 51.CombinationSumIII/combination-sum-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        if(n<k) return {};
+        vector<int> v={1,2,3,4,5,6,7,8,9};
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findCombo(0,n,v,ds,ans,k);
+        return ans;
+    }
+    void findCombo(int ind, int target,vector<int>& v, vector<int>& ds,vector<vector<int>>& ans, int& k){
+        if(target<0) return;
+        if(k==0 && target==0){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=ind;i<9;i++){
+            ds.push_back(v[i]);
+            findCombo(i+1,target-v[i],v,ds,ans,--k);
+            ds.pop_back();
+            k++;
+        }
+        
+    }
+};
