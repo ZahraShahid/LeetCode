5be0692e1bd21e35ef9dde5105a89ead82fb3b3f
Zahra Shahid <zashahid45@gmail.com>
2022-05-16 23:39:47 +0500

    Sheet D1 Easy

---
+++ DSA-SHEET/Arrays/move-zeroes.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/move-zeroes/
+
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int non_zero_index = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[non_zero_index] = nums[i];
+                non_zero_index++;
+            }
+        }
+        for (int i = non_zero_index; i < nums.size(); i++)
+        {
+            nums[i] = 0;
+        }
+    }
+};
